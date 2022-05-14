3f714df80b7275affc21c1f2929c72dc7325bcc8
anurag <anuragraushan373@gmail.com>
2022-05-14 16:55:01 +0530

    done

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,31 @@
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBMfHUVXe4GDHryY7sJMPmnElS5kLDZZo4",
+//   authDomain: "crypto-hunter-50991.firebaseapp.com",
+//   projectId: "crypto-hunter-50991",
+//   storageBucket: "crypto-hunter-50991.appspot.com",
+//   messagingSenderId: "141181984273",
+//   appId: "1:141181984273:web:ba51a25e27ce6a2b1d9bb0",
+// };
+
+// export default firebaseConfig;
+
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDuQ76SVkzuvKLOej-nbm9jSITf3v7QBfY",
+  authDomain: "crypto-tracking-app-4159a.firebaseapp.com",
+  projectId: "crypto-tracking-app-4159a",
+  storageBucket: "crypto-tracking-app-4159a.appspot.com",
+  messagingSenderId: "413083593964",
+  appId: "1:413083593964:web:226496b411c12f5aedc9c9"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
